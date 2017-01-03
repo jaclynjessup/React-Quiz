6a17b393c867c1edf12954a43f0a21d0f2bfad83
jaclynjessup <jaclynjmoore@gmail.com>
2017-01-03 16:12:35 -0500

    Edited after comments from submission

---
+++ react/src/main.js
@@ -0,0 +1,13 @@
+import './main.scss';
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js'
+import data from '../src/constants/data';
+
+$(function() {
+  ReactDOM.render(
+    <App data={data}/>,
+    document.getElementById('app')
+  );
+});
